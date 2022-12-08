0c9cdb5d2091476cd42b2fd523b42b52e7c8dd40
Tori1430 <119774821+Tori1430@users.noreply.github.com>
2022-12-07 21:49:18 -0500

    Create Principal.py

--- Principal.py
+++ Principal.py
@@ -4,22 +4,6 @@ import numpy as np
 import datetime
 from utilities import *
 
-#####
-#Título
-st.title('Fallecidos,hospitalizados y vacunados por COVID-19')
-
-st.image("https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2021/03/06/uci.jpeg", width=600)
-st.subheader("Miembros del equipo")
-st.markdown("""
-- Lorena Belen Cerda Rioja 
-- Anel Schantal Ortiz Camargo
-- Flor Estefany Chuchullo Mosqueta
-- Victoria Justa Navarro Valdiviezo
-- Licie Solainch Paredes Bedoya
-""")
-st.subheader("Contexto Nacional e Internacional")
-st.markdown("La pandemia debida a enfermedad por coronavirus 2019 (COVID-19) ha producido más de 70 mil muertes en el mundo.")
-
 # Creación del dataframe con los datos "preprocesados"
 download_data('data.csv')
 df = pd.read_csv('data.csv', encoding='utf-8')
@@ -28,6 +12,17 @@ seleccion = ['fecha_fallecimiento',
         'dpt_cdc', 'cdc_positividad', 'flag_vacuna',
         'flag_hospitalizado', 'flag_uci', 'con_oxigeno',
         'con_ventilacion', 'evolucion_hosp_ultimo']
+
+"""
+        Edad
+        Positividad
+        Vacuna
+        Hospitalizado
+        UCI
+        Oxígeno
+        Ventilación
+        Evolución
+"""
 
 df = df[seleccion]
 df['fecha_fallecimiento'] = pd.to_datetime(df['fecha_fallecimiento']).dt.date
@@ -38,17 +33,18 @@ set_dep = np.sort(df['dpt_cdc'].dropna().unique())
 dep_opt = st.selectbox('Departamento', set_dep)
 df_dep = df[df['dpt_cdc'] == dep_opt]
 num_filas = df_dep.shape[0]
+num_hosp = df_dep[df_dep['flag_hospitalizado'] == 1].shape[0]
 
+st.write('Número de Fallecidos en el Departamento: ', num_filas)
+st.write('Número de Hospitalizados: ', num_hosp)
 
-st.write('Número de fallecidos en el departamento: ', num_filas)
 
 f_0 = st.date_input("Indique una fecha inferior: ", datetime.date(2020, 1, 1))
 f_f = st.date_input("Indique una fecha superior: ", datetime.date(2021, 1, 1))
-option = st.selectbox('Seleccione el sexo', ('M', 'F', 'Both'))
 
 data = filtered_data(df, f_0, f_f)
-chart(data, option)
-st.write("Cifras de fallecidos posicionado en el mapa geográfico del Perú. ")
 Distribuciones(data)
-st.write("En el gráfico se observa el número de fallecidos por departamento  identificandose el sexo de cada individuo.")
+plot_Criterio(data)
 
+option = st.selectbox('Seleccione el sexo', ('M', 'F', 'Both'))
+chart(data, option)
