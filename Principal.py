8fd39bde249e03050933c9166a56e2ae360a5b80
AnelSchantal <117460619+AnelSchantal@users.noreply.github.com>
2022-12-07 20:28:36 -0500

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -50,6 +50,5 @@ data = filtered_data(df, f_0, f_f)
 
 
 gráfico ( datos , opción ) 
-st.write("En el gráfico se observa el número de fallecidos por departamento  identificandose el sexo de cada individuo: ")
-
 Distribuciones ( datos )
+st.write("En el gráfico se observa el número de fallecidos por departamento  identificandose el sexo de cada individuo: ")
