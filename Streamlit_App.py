ea5260be26712efa96f4dd25199872a596d60bc8
Tori1430 <119774821+Tori1430@users.noreply.github.com>
2022-12-08 20:41:05 -0500

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -9,38 +9,6 @@ st.set_page_config(
     layout="wide",
 )
 
-#####
-#Título
-st.title('Fallecidos, hospitalizados y vacunados por COVID-19')
-
-st.image("https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2021/03/06/uci.jpeg", width=600)
-st.subheader("Miembros del equipo")
-st.markdown("""
-- Lorena Belen Cerda Rioja 
-- Anel Schantal Ortiz Camargo
-- Flor Estefany Chuchullo Mosqueta
-- Victoria Justa Navarro Valdiviezo
-- Licie Solainch Paredes Bedoya
-""")
-
-st.markdown("## ")
-
-st.subheader("Contexto Nacional e Internacional")
-st.markdown("La pandemia debida a enfermedad por coronavirus 2019 (COVID-19) ha producido más de 70 mil muertes en el mundo.")
-
-st.markdown("## ")
-
-#DATASET
-st.subheader("Dataset")
-st.markdown("Toma como referencia el universo de fallecidos por covid, vinculando información de aquellos que estuvieron hospitalizados y si han recibido dosis de vacunas covid.") 
-st.markdown("Los datos analizados en esta página fueron recuperados de la ´Plataforma Nacional de Datos Abiertos´ del Ministerio de Salud (MINSA).")
-"En total fueron 32 variables, de los cuales solo escogimos # debido a la relación con nuestro dashboard."
-st.markdown("Última fecha de modificación : 22 de diciembre del 2021")
-st.markdown("URL: https://www.datosabiertos.gob.pe/dataset/fallecidos-hospitalizados-y-vacunados-por-covid-19")
-st.image("https://consultas-dev.sc.minsa.mvpdemoapp.com/assets/logo.png", width=250)
-
-st.markdown("## ")
-
 # Creación del dataframe con los datos "preprocesados"
 download_data('data.csv')
 df = pd.read_csv('data.csv', encoding='utf-8')
@@ -61,11 +29,18 @@ with fig_col1:
     set_dep = np.sort(df['dpt_cdc'].dropna().unique())
     dep_opt = st.selectbox('Departamento', set_dep)
     df_dep = df[df['dpt_cdc'] == dep_opt]
+
     num_filas = df_dep.shape[0]
     num_hosp = df_dep[df_dep['flag_hospitalizado'] == 1].shape[0]
+    num_O2 = df_dep[df_dep['con_oxigeno'] > 0].shape[0]
+    num_vacunados = df_dep[df_dep['flag_vacuna'] > 0].shape[0]
+    num_ventilacion = df_dep[df_dep['con_ventilacion'] > 0].shape[0]
 
     st.write('Número de Fallecidos en el Departamento: ', num_filas)
     st.write('Número de Hospitalizados: ', num_hosp)
+    st.write('Número de Hospitalizados con Oxígeno: ', num_O2)
+    st.write('Número de Hospitalizados con Oxígeno: ', num_ventilacion)
+    st.write('Número de Vacunados: ', num_vacunados)
 
 
 with fig_col2:
@@ -84,18 +59,40 @@ with fig_col1:
 
 data = filtered_data(df, f_0, f_f, Lima_exclude)
 
+with fig_col1:
+    st.write('Número de fallecidos: ', data.shape[0])
+
 with fig_col2:
-        Distribuciones(data)
+    Distribuciones(data)
 
 fig_col1, fig_col2 = st.columns(2)
 
 with fig_col1:
     plot_Criterio(data)
 
+st.markdown("## Mapa geográfico (afecta el límite de fechas)")
 fig_col1, fig_col2 = st.columns(2)
 
 with fig_col1:
-    option = st.selectbox('Seleccione el sexo', ('M', 'F', 'Both'))
+    option = st.selectbox('Seleccione el sexo', ('Masculino', 'Femenino', 'Ambos'))
+    df = data.copy()
+    if option == 'Masculino':
+        df = df[df['sexo'] == 'M']
+    elif option == 'Femenino':
+        df = df[df['sexo'] == 'F']
+    hosp = st.checkbox('Restringir a Hospitalizados')
+    if hosp:
+        df = df[df['flag_hospitalizado'] == 1]
+    oxig = st.checkbox('Restringir a los que requirieron Oxigeno')
+    if oxig:
+        df = df[df['con_oxigeno'] > 0]
+    vent = st.checkbox('Restringir a los que requirieron Ventilación')
+    if vent:
+        df = df[df['con_ventilacion'] > 0]
+    vac = st.checkbox('Restringir a Vacunados')
+    if vac:
+        df = df[df['flag_vacuna'] > 0]
+    st.write('Número de fallecidos en la gráfica: ', df.shape[0])
 
 with fig_col2:
-    chart(data, option)
+    chart(df)
